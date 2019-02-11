13e0c9a8fda6ca13c273a4d15d11bb02af8e1712
Aidan Drzewicki <02aidan.drzewicki@gulllakecs.org>
2019-02-11 09:54:53 -0500

    Add a Greetings Function

---
+++ Functions-GIT.py
@@ -0,0 +1,11 @@
+# Aidan Drzewicki
+# 4.13.3: Greeting
+# 2.6.19
+
+name = input("What is your name: ")
+
+def greeting():
+    print("Hi there " + name + "!")
+    print("Nice to meet you!")
+
+greeting()
