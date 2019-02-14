156972542bf0d698493662c4a95f05678d1ec650
Aidan Drzewicki <02aidan.drzewicki@gulllakecs.org>
2019-02-14 09:33:45 -0500

    Add Functions & Variables

--- Functions-GIT.py
+++ Functions-GIT.py
@@ -2,6 +2,7 @@
 # 4.13.3: Greeting
 # 2.6.19
 
+'''
 name = input("What is your name: ")
 
 def greeting():
@@ -9,3 +10,17 @@ def greeting():
     print("Nice to meet you!")
 
 greeting()
+'''
+
+# 4.13.4: Functions and Variables
+# Aidan Drzewicki
+# 2.14.19
+
+x = 69
+
+def print_something():
+    x = 13
+    print(x)
+
+print_something()
+print(x)
