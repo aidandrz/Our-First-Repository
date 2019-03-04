04fcae0da0c8c1c05439655bd2ff072cf8f150d6
aidandrz <48022320+aidandrz@users.noreply.github.com>
2019-03-04 09:09:59 -0500

    Add Enter a Number

--- Functions-GIT.py
+++ Functions-GIT.py
@@ -96,3 +96,18 @@ def print_multiple_times(string, times):
         print(string)
 
 print_multiple_times('\n''get the bread gamers', 500)
+
+
+
+# 4.16.3: Enter a Number
+# Aidan Drzewicki
+# 2.20.19
+
+
+
+try:
+    my_number = int(input('Enter an integer: '))
+    print('Your number: ' + str(my_number))
+
+except ValueError:
+    print('That was not an integer')
